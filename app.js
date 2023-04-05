b9ad8c91a633e3ef0794eb852ff403520ba0c868
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-05 20:58:59 +0400

    54

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 function hanoiTower(num, fromRod, toRod, auxRod) {
   if (num === 1) {
+
+
+
     console.log(`Move disk 1 from rod ${fromRod} to rod ${toRod}`);
     return;
   }
+
+
+
+
+
 
   hanoiTower(num - 1, fromRod, auxRod, toRod);
 
