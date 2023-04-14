07b3be285bb08230fb644fccfc3212cf35d302a9
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-14 17:03:39 +0400

    fx7

--- test/sometest.test.ts
+++ test/sometest.test.ts
@@ -1,7 +1,1 @@
-
-
-<<<<<<< Updated upstream
-// example of test.only() that will fail the pipeline
-=======
 // example of tsest.only() that will fail the pipeline
->>>>>>> Stashed changes
--- test/sub/sub.test.ts
+++ test/sub/sub.test.ts
@@ -1,5 +1,1 @@
-<<<<<<< Updated upstream
-// example of test.only() that will fail the pipeline
-=======
 // example of tesst.only() that will fail the pipeline
->>>>>>> Stashed changes
