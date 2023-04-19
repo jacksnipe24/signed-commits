c9033d814667c002494b4ac009e1422913d5215b
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-19 19:52:16 +0400

    test

--- scripts.sh
+++ scripts.sh
@@ -1,11 +1,35 @@
-set +e
-files=$(find ./test -name "*.test.ts" | xargs grep -l 'test.only')
-set -e
+dependency="@chainlink/external-adapter-framework"
+latest_version=0.28.5
+packages=$(find packages -name 'package.json')
+v3_packages=()
+outdated_packages=()
+changes=($(git diff --name-only HEAD^ HEAD | xargs -I{} dirname {} | sort -u))
 
-if [[ -n $files ]]; then
-  echo "Error: Found /test.only/ in one of the following test files:"
-  echo "$files"
+# get the list of v3 EAs
+for file in $(echo $packages); do
+  if jq -e ".dependencies[\"$dependency\"]" "$file" >/dev/null; then
+    v3_packages+=("$(dirname $file)")
+  fi
+done
+
+
+# check if changed code is part of v3 EA
+for changedFile in "${changes[@]}"; do
+  for package in "${v3_packages[@]}"; do
+    if [[ "$changedFile" == "$package"* ]]; then
+      # found change in v3 adapter, comparing versions
+      local_version=$(jq -r ".dependencies[\"$dependency\"]" "$package/package.json")
+      if [ "$local_version" != $(echo $latest_version) ]; then
+        outdated_packages+=("$package")
+      fi
+    fi
+  done
+done
+
+if [ ${#outdated_packages[@]} -gt 0 ]; then
+  echo "The following packages have an outdated \"$dependency\" dependency:"
+  for file in "${outdated_packages[@]}"; do
+    echo "- $file"
+  done
   exit 1
-else
-  echo "No instances of /test.only/ found in test files"
 fi
