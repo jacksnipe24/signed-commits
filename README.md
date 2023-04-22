ed567d0e30a7d584f3db2cfe60ff186ec8441d65
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-22 22:33:58 +0400

    afjasf2

---
+++ README.md
@@ -0,0 +1,8 @@
+# Signed commits
+
+![Coverage](https://img.shields.io/badge/coverage-100%25-green)
+
+
+> **Warning**
+> warning text
+
---
+++ script.sh
@@ -0,0 +1,3 @@
+coverage=95
+
+sed -i '' 's/\(!\[Coverage\](https:\/\/img\.shields\.io\/badge\/coverage-[0-9]\{1,3\}%25-green)\)/![Coverage](https:\/\/img\.shields\.io\/badge\/coverage-'$coverage'%25-green)/g' README.md
