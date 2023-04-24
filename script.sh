5384e61be5390c65c46945b324bbd47da1ce4138
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-24 18:01:47 +0400

    sjs72hs

--- script.sh
+++ script.sh
@@ -1,6 +1,9 @@
-coverage=98
 readme="README.md"
 
-awk '{gsub(/!\[Coverage\]\(https:\/\/img\.shields\.io\/badge\/coverage-[0-9]{1,3}%25-green\)/,"![Coverage](https://img.shields.io/badge/coverage-'$coverage'%25-green)")}1' "$readme" > tmp && mv tmp "$readme"
+yarn test
+
+new_coverage=$(jq '.[].s | map(select(. > 0)) | length * 100 / length' coverage/coverage-final.json)
+
+awk '{gsub(/!\[Coverage\]\(https:\/\/img\.shields\.io\/badge\/coverage-[0-9]{1,3}%25-green\)/,"![Coverage](https://img.shields.io/badge/coverage-'$new_coverage'%25-green)")}1' "$readme" > tmp && mv tmp "$readme"
 
 
