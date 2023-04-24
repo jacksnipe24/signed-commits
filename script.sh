5b7da56d8aac649e069c4a83c66aa498ca86a53d
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-24 12:28:57 +0400

    sjh29dn

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-coverage=95
+coverage=98
 readme="README.md"
 
 awk '{gsub(/!\[Coverage\]\(https:\/\/img\.shields\.io\/badge\/coverage-[0-9]{1,3}%25-green\)/,"![Coverage](https://img.shields.io/badge/coverage-'$coverage'%25-green)")}1' "$readme" > tmp && mv tmp "$readme"
