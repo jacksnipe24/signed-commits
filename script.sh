0aa0973b8d03208c1ddd4516488c23d842917c1b
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-25 20:04:58 +0400

    ssh38shs

--- script.sh
+++ script.sh
@@ -1,3 +1,4 @@
-FILE_TO_COMMIT="README.md"
-NEW_COVERAGE=18
-awk '{gsub(/!\[Coverage\]\(https:\/\/img\.shields\.io\/badge\/coverage-[0-9]{1,3}%25-green\)/,"![Coverage](https://img.shields.io/badge/coverage-'$NEW_COVERAGE'%25-green)")}1' "$FILE_TO_COMMIT" >tmp && mv tmp "$FILE_TO_COMMIT"
+COVERAGE_STATEMENTS=$(cat coverage/coverage-summary.json | jq '.total.statements.pct | round')
+BRANCH="coverage-pr-$COVERAGE_STATEMENTS"
+
+echo $BRANCH
