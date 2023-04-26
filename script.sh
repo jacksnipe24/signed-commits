dec5be907a6fc5d76482f8cc7161ab35625709a6
jacksnipe24 <kastepanyan24@gmail.com>
2023-04-26 19:10:10 +0400

    sj8ss

--- script.sh
+++ script.sh
@@ -1,4 +1,18 @@
-COVERAGE_STATEMENTS=$(cat coverage/coverage-summary.json | jq '.total.statements.pct | round')
-BRANCH="coverage-pr-$COVERAGE_STATEMENTS"
+#!/bin/bash
 
-echo $BRANCH
+# Assign the string to a variable
+my_string="
+# Signed commits
+
+![Coverage](https://img.shields.io/badge/coverage-22.3%25-green)
+
+
+> **Warning**
+> warning text
+
+"
+
+coverage=$(echo "$my_string" | grep -o 'coverage-[0-9]*\(\.[0-9]\{1,\}\)\?%' | sed 's/coverage-\(.*\)%/\1/')
+
+# Print the extracted value
+echo "Coverage: $coverage"
