336b632e4f36cd6f2c8782b0cb0ac98ea3a7c242
jacksnipe24 <kastepanyan24@gmail.com>
2023-05-09 12:46:16 +0400

    ne11

--- script.sh
+++ script.sh
@@ -12,6 +12,7 @@ IFS='.' read -r -a new <<< "$versionNew"
 HIGHEST_VERSION="equal"
 
 # Compare major version number
+
 if (( bump[0] > new[0] )); then
   HIGHEST_VERSION="bump"
 elif (( bump[0] < new[0] )); then
